ee00798ff16d6f71645745ace77c1ddcefa81e55
nicholaslum444 <nicholaslum444@hotmail.com>
2014-06-13 17:22:41 +0800

    Added win-checking logic and slight refactoring.
    The game now stops when a player has won. The player's color will fill the
    game board and the game board will flash. Still don't know how to draw the
    line across the board to indicate the win location. Also some refactoring
    but it's not important.
    
    Signed-off-by: nicholaslum444 <nicholaslum444@hotmail.com>

--- src/com/nick/nictactoe/GamePage.java
+++ src/com/nick/nictactoe/GamePage.java
@@ -2,6 +2,7 @@ package com.nick.nictactoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -18,9 +19,9 @@ import android.widget.TextView;
  */
 public class GamePage extends Activity {
 	
-	final static int PLAYER_BLUE = 0;
+	final static int PLAYER_BLUE = -1;
 	final static int PLAYER_RED = 1;
-	final static int PLAYER_NONE = -1;
+	final static int PLAYER_NONE = 0;
 	
 	final static String PLAYER_BLUE_ICON = "O";
 	final static String PLAYER_RED_ICON = "X";
@@ -30,10 +31,18 @@ public class GamePage extends Activity {
 	View viewPlayerBlue;
 	View viewPlayerRed;
 	
-	int currentPlayer = -1;
+	int currentPlayer = PLAYER_NONE;
 	int playerWhoStarted;
 	
+	int[][] gameBoardArray;
 	int filledBoxes;
+	int clickedColumnX;
+	int clickedRowY;
+	
+	boolean winRow;
+	boolean winColumn;
+	boolean winUpDiagonal;
+	boolean winDownDiagonal;
 	
 	Animation animation250;
 	Animation animation500;
@@ -44,16 +53,18 @@ public class GamePage extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contents_game_page);
 		
+		gameBoardArray = new int[3][3];
+		
 		animation250 = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
-	    animation250.setDuration(250); // duration - half a second
+	    animation250.setDuration(300); // duration - half a second
 	    animation250.setInterpolator(new LinearInterpolator()); // do not alter animation250 rate
 	    animation250.setRepeatCount(Animation.INFINITE); // Repeat animation250 infinitely
 	    animation250.setRepeatMode(Animation.REVERSE); // Reverse animation250 at the end so the button will fade back in
 		
-	    animation500 = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
+	    animation500 = new AlphaAnimation(1, (float) 0.3); // Change alpha from fully visible to invisible
 	    animation500.setDuration(500); // duration - half a second
 	    animation500.setInterpolator(new LinearInterpolator()); // do not alter animation250 rate
-	    animation500.setRepeatCount(Animation.INFINITE); // Repeat animation250 infinitely
+	    animation500.setRepeatCount(9); // Repeat animation250 infinitely
 	    animation500.setRepeatMode(Animation.REVERSE); // Reverse animation250 at the end so the button will fade back in
 		
 		layoutPageContents = (LinearLayout) findViewById(R.id.Layout_page_contents);
@@ -62,6 +73,7 @@ public class GamePage extends Activity {
 		viewPlayerRed = findViewById(R.id.View_red_player);
 		
 		layoutGameBoard.setAnimation(null);
+		layoutGameBoard.setClickable(true);
 		
 		if (currentPlayer == PLAYER_NONE) {
 			playerWhoStarted = PLAYER_RED;
@@ -102,28 +114,125 @@ public class GamePage extends Activity {
 		filledBoxes++;
 		v.setClickable(false);
 		
+		fillUpArray(v);
+		
 		TextView tv = (TextView) v;
 		if (currentPlayer == PLAYER_RED) {
 			tv.setText(PLAYER_RED_ICON);
 			tv.setTextColor(getResources().getColor(R.color.red));
-			currentPlayer = PLAYER_BLUE;
 		} else if (currentPlayer == PLAYER_BLUE) {
 			tv.setText(PLAYER_BLUE_ICON);
 			tv.setTextColor(getResources().getColor(R.color.blue));
-			currentPlayer = PLAYER_RED;
 		} else {
 			tv.setText("-");
+		}
+		
+		if (isCurrentPlayerWinner()) {
+			currentFlashing.setAnimation(null);
+			layoutGameBoard.startAnimation(animation500);
+			layoutGameBoard.setClickable(false);
+			
+			if (currentPlayer == PLAYER_RED) {
+				layoutGameBoard.setBackgroundColor(getResources().getColor(R.color.red_faded));
+			} else if (currentPlayer == PLAYER_BLUE) {
+				layoutGameBoard.setBackgroundColor(getResources().getColor(R.color.blue_faded));
+			}
+			
+			return;
 		}
 
 		if (filledBoxes == 9) {
 			currentFlashing.setAnimation(null);
 			layoutGameBoard.startAnimation(animation500);
+			layoutGameBoard.setClickable(false);
+			layoutGameBoard.setBackgroundColor(getResources().getColor(R.color.grey_faded));
 			return;
 		}
 		
+		
+		switchPlayer();
 		flashPlayerColor();
 	}
 	
+	public void fillUpArray(View v) {
+		clickedColumnX = Integer.parseInt((String) v.getTag());
+		clickedRowY = Integer.parseInt((String) ((View) v.getParent()).getTag());
+		Log.w("index", clickedColumnX +""+ clickedRowY);
+		gameBoardArray[clickedColumnX][clickedRowY] = currentPlayer;
+	}
+	
+	public boolean isCurrentPlayerWinner() {
+		return checkRow() || checkColumn() || checkUpDiagonal() || checkDownDiagonal();
+	}
+	
+	public boolean checkRow() {
+		for (int i = 0; i < 3; i++) {
+			if (gameBoardArray[i][clickedRowY] != currentPlayer) {
+				return false;
+			}
+		}
+		Log.w("row", "q");
+		winRow=true;
+		return true;
+	}
+	
+	public boolean checkColumn() {
+		for (int i = 0; i < 3; i++) {
+			if (gameBoardArray[clickedColumnX][i] != currentPlayer) {
+				return false;
+			}
+		}
+		Log.w("col", "q");
+		winColumn=true;
+		return true;
+	}
+	
+	public boolean checkUpDiagonal() { /* "/" */
+		// check if within the 3 squares that is possible for diag
+		if ((clickedRowY == 2 && clickedColumnX == 0) ||
+			(clickedRowY == 1 && clickedColumnX == 1) ||
+			(clickedRowY == 0 && clickedColumnX == 2)) {
+				
+			for (int x = 0, y = 2; x < 3; x++, y--) {
+				if (gameBoardArray[x][y] != currentPlayer) {
+					return false;
+				}
+			}
+			Log.w("up", "q");
+			winUpDiagonal=true;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean checkDownDiagonal() { /* "\" */
+		// check if within the 3 squares that is possible for diag
+		if ((clickedRowY == 0 && clickedColumnX == 0) ||
+			(clickedRowY == 1 && clickedColumnX == 1) ||
+			(clickedRowY == 2 && clickedColumnX == 2)) {
+				
+			for (int x = 0, y = 0; y < 3; y++, x++) {
+				if (gameBoardArray[x][y] != currentPlayer) {
+					return false;
+				}
+			}
+			Log.w("down", "q");
+			winDownDiagonal=true;
+			return true;
+		}
+		return false;
+	}
+	
+	public void switchPlayer() {
+		if (currentPlayer == PLAYER_RED) {
+			currentPlayer = PLAYER_BLUE; 
+		} else if (currentPlayer == PLAYER_BLUE) {
+			currentPlayer = PLAYER_RED;
+		}
+	}
+	
+
 	public void newGame(View v) {
 		if (playerWhoStarted == PLAYER_RED) {
 			playerWhoStarted = PLAYER_BLUE;
